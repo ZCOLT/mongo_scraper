62aa7983f1bfc1d6b7adb70a57b796cee0523fd5
ZCOLT <colton.zac@gmail.com>
2018-09-18 18:51:07 -0600

    fixed mongodb connection

--- server.js
+++ server.js
@@ -36,6 +36,13 @@ app.set("view engine", "handlebars");
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/nytimes_db", { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 
